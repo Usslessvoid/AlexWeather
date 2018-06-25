534844488d6f64b4ce7de0908998b52249c95763
Usslessvoid <36482404+Usslessvoid@users.noreply.github.com>
2018-06-25 18:02:08 +0300

    Initial commit

---
+++ AlexWeather/app/src/main/java/com/example/drcreeper/alexweather/fragments/LocationChooseFragment.java
@@ -0,0 +1,80 @@
+package com.example.drcreeper.alexweather.fragments;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.drcreeper.alexweather.AddLocationActivity;
+import com.example.drcreeper.alexweather.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+// api key: 5753974d4f4e8e428ba77f4eb8816501
+
+public class LocationChooseFragment extends Fragment {
+
+    @BindView(R.id.location_list)
+    ListView locationsList;
+    @BindView(R.id.no_items)
+    View v;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main,container,false);
+        setHasOptionsMenu(true);
+        ButterKnife.bind(this,view);
+        locationsList.setEmptyView(v);
+
+
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.main_menu,menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.refresh_button:
+                refresh();
+                break;
+            case R.id.add_button:
+                add();
+                break;
+        }
+        return true;
+    }
+
+    public void refresh(){
+
+    }
+
+    public void add(){
+        Intent addLocation = new Intent(getContext(), AddLocationActivity.class);
+        getActivity().startActivity(addLocation);
+    }
+    @OnClick(R.id.create_location_button)
+    public void onAddPress(){
+        add();
+    }
+}
---
+++ AlexWeather/app/src/main/java/com/example/drcreeper/alexweather/models/LocationItem.java
@@ -0,0 +1,15 @@
+package com.example.drcreeper.alexweather.models;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class LocationItem {
+
+    private int id;
+    private String name;
+    private int temperature;
+    private String state;
+    private Date lastUpdate;
+    private int townId;
+
+}
